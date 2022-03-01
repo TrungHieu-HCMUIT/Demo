799044adc6101ab219507e21d7ecdba87ae055e7
Đức Trung Hiếu Võ <ductrunghieuvo@Khong-phai-MacBook-Pro.local>
2022-03-01 17:58:12 +0700

    DONE

---
+++ js/myJs.js
@@ -0,0 +1,177 @@
+// Main
+const textConfig = {
+  text1: "CAUTION: This site may contains cheesy content",
+  text2: "Helu Ngố, mình có điều này muốn hỏi, cần cậu trả lời thật lòng",
+  text3: "Cậu có nhớ mình không?",
+  text4: "Nếu không trả lời mà thoát ra thì mặc định là nhớ đó",
+  text5: "Không",
+  text6: "Có",
+  text7: "Title",
+  text8: "Gửi",
+  text9: "Nothing",
+  text10: "Title",
+  text11:
+    'Content content content',
+  text12: "Done",
+};
+
+$(document).ready(function () {
+  // process bar
+  setTimeout(function () {
+    firstQuestion();
+    $(".spinner").fadeOut();
+    $("#preloader").delay(350).fadeOut("slow");
+    $("body").delay(350).css({
+      overflow: "visible",
+    });
+  }, 600);
+
+  $("#text3").html(textConfig.text3);
+  $("#text4").html(textConfig.text4);
+  $("#no").html(textConfig.text5);
+  $("#yes").html(textConfig.text6);
+
+  function firstQuestion() {
+    $(".content").hide();
+    Swal.fire({
+      title: textConfig.text1,
+      // text: textConfig.text2,
+      imageUrl: "img/main_pic.jpeg",
+      imageWidth: 215,
+      showCancelButton: false,
+      confirmButtonColor: "#fe8a71",
+      confirmButtonText: "Continue",
+      background: 'url("img/input-bg.jpeg")',
+      imageAlt: "Custom image"
+    }).then(function () {
+      $(".content").show(210);
+      playSound();
+    });
+  }
+
+  function playSound() {
+    var audio = new Audio("sound/sound.mp3");
+    audio.volume = 0.6;
+    audio.play();
+    if (typeof audio.loop == 'boolean') {
+      audio.loop = true;
+    }
+    else {
+      audio.addEventListener('ended', function () {
+        this.currentTime = 0;
+        this.play();
+      }, false);
+    }
+  }
+
+  // switch button position
+  function switchButton() {
+    // var audio = new Audio("sound/duck.mp3");
+    // audio.play();
+    var leftNo = $("#no").css("left");
+    var topNO = $("#no").css("top");
+    var leftY = $("#yes").css("left");
+    var topY = $("#yes").css("top");
+    $("#no").css("left", leftY);
+    $("#no").css("top", topY);
+    $("#yes").css("left", leftNo);
+    $("#yes").css("top", topNO);
+  }
+  // move random button póition
+  function moveButton() {
+    // var audio = new Audio("sound/Swish1.mp3");
+    // audio.play();
+    if (screen.width <= 600) {
+      var x = Math.random() * 300;
+      var y = Math.random() * 500;
+    } else {
+      var x = Math.random() * 500;
+      var y = Math.random() * 500;
+    }
+    var left = x + "px";
+    var top = y + "px";
+    $("#no").css("left", left);
+    $("#no").css("top", top);
+  }
+
+  var n = 0;
+  $("#no").mousemove(function () {
+    if (n < 1) switchButton();
+    if (n > 1) moveButton();
+    n++;
+  });
+  $("#no").click(() => {
+    if (screen.width >= 900) switchButton();
+  });
+
+  // generate text in input
+  function textGenerate() {
+    var n = "";
+    var text = " " + textConfig.text9;
+    var a = Array.from(text);
+    var textVal = $("#txtReason").val() ? $("#txtReason").val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+      for (let i = 1; i <= count; i++) {
+        n = n + a[i];
+        if (i == text.length + 1) {
+          $("#txtReason").val("");
+          n = "";
+          break;
+        }
+      }
+    }
+    $("#txtReason").val(n);
+  }
+
+  // show popup
+  $("#yes").click(function () {
+    // var audio = new Audio("sound/tick.mp3");
+    // audio.play();
+    Swal.fire({
+      title: textConfig.text7,
+      // html: true,
+      input: 'text',
+      width: 900,
+      padding: "2em",
+      // html: "<input type='text' class='form-control' style='height:100px' id='txtReason'  placeholder=' '>",
+      background: '#F8F8F8 url("img/puuung_landscape.png")',
+      // backdrop: `
+      //               rgba(0,0,123,0.4)
+      //               url("img/giphy2.gif")
+      //               left top
+      //               no-repeat
+      //             `,
+      showCancelButton: false,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonColor: "#fe8a71",
+      cancelButtonColor: "#f6cd61",
+      confirmButtonText: textConfig.text8,
+      customClass: "swal-image-yes",
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          width: '90%',
+          confirmButtonText: textConfig.text12,
+          background: '#F8F8F8',
+          title: textConfig.text10,
+          text: textConfig.text11,
+          confirmButtonColor: "#83d0c9",
+          onClose: () => {
+            window.location = "https://www.facebook.com/hieeu1.7/";
+          },
+        })
+      }
+    });
+
+    // $("#txtReason").focus(function () {
+    //   var handleWriteText = setInterval(function () {
+    //     textGenerate();
+    //   }, 10);
+    //   $("#txtReason").blur(function () {
+    //     clearInterval(handleWriteText);
+    //   });
+    // });
+  });
+});
